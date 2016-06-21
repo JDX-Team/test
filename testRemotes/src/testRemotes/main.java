6ace4e9c836a24a7d96386b8cc6f7dcf8f95fe76
JFH <josfelher@gmail.com>
2016-06-21 17:19:29 +0200

    limpieza de subidas subida de prueba

--- testRemotes/src/testRemotes/main.java
+++ testRemotes/src/testRemotes/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("subida limpia rama prueba");
+		System.out.println("prueba");
 	}
 
 }
