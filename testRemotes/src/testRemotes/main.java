f97694410ce6cfd2fd2bc4cba3fb4d3a7a8d4a01
JFH <josfelher@gmail.com>
2016-06-21 16:27:53 +0200

    segundo cambio a repo test rama master

--- testRemotes/src/testRemotes/main.java
+++ testRemotes/src/testRemotes/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("primer cambio a remote test rama master");
+		System.out.println("segundo cambio a remote test rama master");
 	}
 
 }
