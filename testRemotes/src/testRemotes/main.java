40903ebab2aec8245ec90aa1ca75fc00c34d5a5c
JFH <josfelher@gmail.com>
2016-06-21 16:01:45 +0200

    primer cambio a remote test rama master

--- testRemotes/src/testRemotes/main.java
+++ testRemotes/src/testRemotes/main.java
@@ -4,7 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("primer cambio a remote prueba rama master");
+		System.out.println("primer cambio a remote test rama master");
 	}
 
 }
