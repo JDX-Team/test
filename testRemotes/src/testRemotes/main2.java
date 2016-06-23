b0d341dc97d04bb9f3bf3cce62aba8c79c55ff72
JFH <josfelher@gmail.com>
2016-06-23 10:34:35 +0200

    cambios implementados para privativo

--- testRemotes/src/testRemotes/main2.java
+++ testRemotes/src/testRemotes/main2.java
@@ -1,5 +1,8 @@
 package testRemotes;
 
 public class main2 {
-
+	
+	public main2(){
+		System.out.println("cambios desarrollados en privado2");
+	}
 }
--- testRemotes/src/testRemotes/main3.java
+++ testRemotes/src/testRemotes/main3.java
@@ -1,5 +1,8 @@
 package testRemotes;
 
 public class main3 {
-
+	
+	public main3(){
+		System.out.println("cambios desarrollados en privado3");
+	}
 }
