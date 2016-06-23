6f7f5fb43b4488f57d82cc3d54dad3c3dc3d981b
JFH <josfelher@gmail.com>
2016-06-23 11:14:13 +0200

    cambios para cambio de rama

--- testRemotes/src/testRemotes/main.java
+++ testRemotes/src/testRemotes/main.java
@@ -5,6 +5,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Cambios nuevo evolutivo");
+		System.out.println("Cambios nuevo evolutivo");
+		System.out.println("Cambios nuevo evolutivo");
+		System.out.println("Cambios nuevo evolutivo");
 	}
 
 }
