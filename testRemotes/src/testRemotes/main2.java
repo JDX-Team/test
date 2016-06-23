45e10ce11087e3165137fb3a180364ce145b0eef
JFH <josfelher@gmail.com>
2016-06-23 10:37:23 +0200

    eliminado codigo de clases 2 y 3

--- testRemotes/src/testRemotes/main2.java
+++ testRemotes/src/testRemotes/main2.java
@@ -3,6 +3,6 @@ package testRemotes;
 public class main2 {
 	
 	public main2(){
-		System.out.println("cambios desarrollados en privado2");
+
 	}
 }
--- testRemotes/src/testRemotes/main3.java
+++ testRemotes/src/testRemotes/main3.java
@@ -3,6 +3,6 @@ package testRemotes;
 public class main3 {
 	
 	public main3(){
-		System.out.println("cambios desarrollados en privado3");
+
 	}
 }
